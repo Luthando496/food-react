c3242e5f0180f02cf7dba90f7d5a261c4eb9fe88
unknown <luthandodidiza197@gmail.com>
2023-03-19 12:02:17 +0200

    k

--- src/components/HeadlineCard.jsx
+++ src/components/HeadlineCard.jsx
@@ -11,7 +11,7 @@ const HeadlineCard = () => {
                 <p className='text-2xl px-2 pt-4'>Lorem, 8/27.</p>
                 <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
             </div>
-            <img className='max-h-[160px] md:max-h-[200px] object-cover w-full' src="/src/assets/e32a15868d21a674f3f6f6d8e513a0d6.jpg" alt="img" />
+            <img className='max-h-[160px] md:max-h-[200px] object-cover w-full' src="https://images.pexels.com/photos/1640770/pexels-photo-1640770.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="img" />
         </div>
         <div className='relative rounded-xl ml-5 md:ml-0'>
         {/* overlay */}
@@ -20,7 +20,7 @@ const HeadlineCard = () => {
                 <p className='text-2xl px-2 pt-4'>Lorem, 8/27.</p>
                 <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
             </div>
-            <img className='max-h-[160px] md:max-h-[200px] object-cover w-full' src="/src/assets/eb9bc72d4cd37f8be4357edb465d0178.jpg" alt="img" />
+            <img className='max-h-[160px] md:max-h-[200px] object-cover w-full' src="https://images.pexels.com/photos/1099680/pexels-photo-1099680.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="img" />
         </div>
         <div className='relative rounded-xl ml-5 md:ml-0'>
         {/* overlay */}
@@ -29,7 +29,7 @@ const HeadlineCard = () => {
                 <p className='text-2xl px-2 pt-4'>Lorem, 8/27.</p>
                 <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
             </div>
-            <img className='max-h-[160px] md:max-h-[200px] object-cover w-full' src="/src/assets/e25d66234c6b34875f1add3b40de5c01.jpg" alt="img" />
+            <img className='max-h-[160px] md:max-h-[200px] object-cover w-full' src="https://images.pexels.com/photos/2732663/pexels-photo-2732663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="img" />
         </div>
     </div>
   )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
             {/* overlay */}
             <div className='absolute w-full h-full text-gray-200 max-h-[500px] bg-black/40 flex flex-col justify-center'>
                 <h1 className='px-4 text-4xl sm:text-5xl md:text-7xl font-light'>The <span className='text-yellow-500 font-bold'>Best</span> </h1>
-                <h1 className='px-4 text-4xl sm:text-5xl md:text-7xl font-light'>Brought To <span className='text-yellow-500 font-bold'>Best You</span></h1>
+                <h1 className='px-4 text-4xl sm:text-5xl md:text-7xl font-light'>Fresh Food <span className='text-yellow-500 font-bold'>For You</span></h1>
             </div>
             {/*  */}
             <img src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="background" className='w-full max-h-[500px] object-cover' />
